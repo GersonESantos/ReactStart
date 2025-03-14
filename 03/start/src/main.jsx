a2ef3177994af7510aaa6fd793a2ea3a6cd20365
GersonESS <gebhsantos@gmail.com>
2025-03-14 17:07:15 -0300

    Update main.jsx

--- 03/start/src/main.jsx
+++ 03/start/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App.jsx'
+import { State1 } from './components/State1/index.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <State1 />
   </StrictMode>,
 )
